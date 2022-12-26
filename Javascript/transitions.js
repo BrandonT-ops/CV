68fc1b25811d38f844659342691630a266d64e57
BrandonT-ops <67329075+BrandonT-ops@users.noreply.github.com>
2022-12-26 04:17:14 +0100

    adding functionalities

---
+++ Javascript/transitions.js
@@ -0,0 +1,12 @@
+function moveToHobbies(params) {
+    document.getElementById("moveToHobbies").scrollIntoView({behavior: 'smooth'});
+}
+
+function moveToMyContactDetails(params) {
+    document.getElementById("contactDetails").scrollIntoView({behavior: 'smooth'});
+
+}
+
+function moveToSendAMessage(params) {
+    document.getElementById("contactDetails").scrollIntoView({behavior: 'smooth'});
+}
